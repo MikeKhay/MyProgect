abb7db35abfc1d43dc884afa1c07e705cefadbb2
MikeKhay <khajmike@gmail.com>
2020-11-24 20:56:56 +0200

    Base Code

---
+++ src/main/java/com/example/MyProgect/MyProgectApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyProgect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProgectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProgectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyProgect/MyProgectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyProgect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProgectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
